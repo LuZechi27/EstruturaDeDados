3b636a7b0b6656b31b99f8d72e710e376f23306c
Luiz Gustavo Silva Zechi <Luzechi27@gmail.com>
2026-03-13 08:18:30 -0300

    finished queue

--- src/fila/fila.c
+++ src/fila/fila.c
@@ -1,35 +1,85 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include<stdlib.h>
 #include "fila.h"
 
-#define MAX_TAM 5
+#define MAXTAM 5
+#define ERROR 0
+#define SUCCESS 
 
-typedef struct fila
-{
-    int item[MAX_TAM];
+struct fila {
+    int item[MAXTAM];
     int inicio, fim, qtd;
-} Fila;
+};
 
-Fila *new_fila() {
-    Fila *f = (Fila*) malloc(sizeof(Fila));
-    if (f != NULL) {
-        f->inicio = 0;
-        f->fim = -1;
-        f->qtd = 0;
-    }
+Fila *new_queue() {
+    Fila *f = ( Fila * ) malloc( sizeof( Fila ) );
+    if (f == NULL)
+        return NULL;
+
+    f->inicio = 0;
+    f->fim = -1;
+    f->qtd = 0;
     return f;
 }
 
-int fila_cheia(Fila *f) {
-    return f->qtd == MAX_TAM;
+int queue_is_full( Fila *f ) {
+    return f->qtd == MAXTAM;
 }
 
-int fila_vazia(Fila *f) {
+int queue_is_empty( Fila *f ) {
     return f->qtd == 0;
 }
 
-int enfileirar(Fila *f, int valor) {
-    if (fila_cheia(f)) {
-        
+// enqueue - Complexidade constante O(1)
+int enqueue( Fila *f, int valor ) {
+    if (filaCheia( f ))
+        return ERROR;
+    else {
+       // f->fim++;
+       // if (f->fim==MAXTAM)
+       //    f->fim=0;
+        f->fim = ( f->fim + 1 ) % MAXTAM;
+        f->item[f->fim] = valor;
+        f->qtd++;
+        return SUCCESS;
     }
+}
+
+// dequeue = Complexidade constante O(1)
+int dequeue( Fila *f, int *valor ) {
+    if (filaVazia( f ))
+        return ERROR;
+    else {
+        *valor = f->item[f->inicio];
+        f->inicio = ( f->inicio + 1 ) % MAXTAM;
+        f->qtd--;
+        return SUCCESS;
+    }
+}
+
+// Complexidade constante O(1)
+int get_first( Fila *f, int *valor ) {
+    if (filaVazia( f ))
+        return ERROR;
+    else {
+        *valor = f->item[f->inicio];
+        return SUCCESS;
+    }
+}
+
+// Complexidade linear O(n)
+void show_queue( Fila *f ) {
+    if (filaVazia( f ))
+        printf( "Fila vazia" );
+    else {
+        int i = f->inicio;
+        for (int cont = 0;cont < f->qtd;cont++) {
+            printf( "Item: %d\n", f->item[i] );
+            i = ( i + 1 ) % MAXTAM;
+        }
+    }
+}
+
+void free_queue( Fila *f ) {
+    free( f );
 }
--- src/fila/fila.h
+++ src/fila/fila.h
@@ -1,4 +1,10 @@
 typedef struct fila Fila;
 
-Fila *new_fila();
-
+Fila *new_queue();
+int queue_is_full( Fila *f );
+int queue_is_empty( Fila *f );
+int enqueue( Fila *f, int valor );
+int dequeue( Fila *f, int *valor );
+int get_first( Fila *f, int *valor );
+void show_queue( Fila *f );
+void free_queue( Fila *f );
